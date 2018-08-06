4433979052813131dfe86386ee172b3dacb6931d
Matthew Lee <matthew_lee@qbsol.com>
2018-08-06 12:23:21 -0500

    Work Queue - initial setup

---
+++ WorkQueues/NewTask/NewTask.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+
+namespace NewTask
+{
+    public class NewTask
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+                    var properties = channel.CreateBasicProperties();
+                    properties.Persistent = true;
+
+                    for(int i = 0; i < 10; ++i)
+                    {
+                        channel.BasicPublish(exchange: "",
+                            routingKey: "task_queue",
+                            basicProperties: properties,
+                            body: body);
+                    }
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "Hello World!");
+        }
+    }
+}
---
+++ WorkQueues/Worker/Worker.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    public class Worker
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "task_queue",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+
+                        int dots = message.Split('.').Length - 1;
+                        Thread.Sleep(dots * 1000);
+
+                        Console.WriteLine(" [x] Done");
+                    };
+
+                    channel.BasicConsume(queue: "task_queue",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
