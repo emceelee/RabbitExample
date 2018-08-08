ed18c6d896ff29a7cf7911c8f09439e05a1dffcd
Matthew Lee <matthew_lee@qbsol.com>
2018-08-08 15:39:54 -0500

    Publish Subscribe tutorial

---
+++ PublishSubscribe/EmitLog/EmitLog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+
+namespace EmitLog
+{
+    class EmitLog
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Declare exchange
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "logs",
+                        routingKey: "",
+                        basicProperties: null,
+                        body: body);
+
+                    Console.WriteLine(" [x] Sent {0}", message);
+                }
+            }
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+        }
+    }
+}
---
+++ PublishSubscribe/ReceiveLogs/ReceiveLogs.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Text;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace ReceiveLogs
+{
+    class ReceiveLogs
+    {
+        public static void Main()
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    //Declare exchange
+                    channel.ExchangeDeclare("logs", "fanout");
+
+                    //Declare queue - default non-durable, exclusive, autodelete, generated name
+                    var queueName = channel.QueueDeclare().QueueName;
+                    channel.QueueBind(queue: queueName,
+                                      exchange: "logs",
+                                      routingKey: "");
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] {0}", message);
+                    };
+
+                    //autoAck = false - message not lost if consumer dies while processing
+                    channel.BasicConsume(queue: queueName,
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    Console.WriteLine("Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
